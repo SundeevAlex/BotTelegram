8987a89f1aa1b706b6777f0fadbb03c0b6d26e79
Aleksandr Sundeev <navigator97@mail.ru>
2024-02-09 14:11:45 +0700

    finished

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+import time
+
 import requests, json, datetime, os, telebot
 
-CURRENCY_RATES_FILE = "currency_rates.json"
+CURRENCY_RATES_FILE = "currency_rates_eur.json"
 BOT_TOKEN = os.getenv("TELEGRAM_KEY")
 CHAT_ID = '5462477537'  #Мой бот
 # CHAT_ID = '1270483208' # Оксана
@@ -9,37 +11,34 @@ API_KEY = os.getenv("APILAYER_KEY")
 
 def main():
     while True:
-        # print(BOT_TOKEN)
-        # print(API_KEY)
-        # currency = input("Введите название валюты (USD или EUR): ").upper()
         currency = 'EUR'
-        if currency not in ["USD", "EUR"]:
-            print("Некорректный ввод")
-            break
-            # continue
         rate = get_currency_rate(currency)
         # print(rate)
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # print(timestamp)
         result = f"Курс {currency} к рублю: {rate:.2f}"
         print(result)
         date = {"currency": currency, "rate": rate, "timestamp": timestamp}
-        print(date)
-        if date != save_to_json(date):
-            print('ОТПРАВЛЯЕТ В ТЕЛЕГУ')
+        date1 = load_from_json(CURRENCY_RATES_FILE)
+        if date["rate"] != date1["rate"]:
+            print('Курс изменился!')
+            print(result)
             send_telegram_message(result)
-
-        data = {"currency": currency, "rate": rate, "timestamp": timestamp}
-        save_to_json(data)
-
-        # choice = input("Выберите действие: (1 - продолжить, 2 - выйти) ")
-        choice = '2'
-        if choice == "1":
-            continue
-        elif choice == "2":
-            break
+            data = {"currency": currency, "rate": rate, "timestamp": timestamp}
+            save_to_json(data)
         else:
-            print("Некорректный ввод")
+            print('Курс не изменился')
+
+        for i in range(1, 5):
+            print_time()
+            time.sleep(60)
+
+        # choice = '2'
+        # if choice == "1":
+        #     continue
+        # elif choice == "2":
+        #     break
+        # else:
+        #     print("Некорректный ввод")
 
 
 def send_telegram_message(my_text):
@@ -48,9 +47,7 @@ def send_telegram_message(my_text):
         'chat_id': CHAT_ID,
         'text': my_text
     }
-    # print('payload=', payload)
     responce = requests.post(url, json=payload)
-    # print('resp=', responce)
     return responce
 
 
@@ -60,7 +57,7 @@ def get_currency_rate(currency: str) -> float:
     response = requests.get(url)
     response_data = response.json()
     rate = response_data['Valute'][currency]['Value']
-    # rate = 97.444
+    # rate = 99.444
     return rate
 
 
@@ -69,11 +66,20 @@ def save_to_json(data: dict) -> None:
         if os.stat(CURRENCY_RATES_FILE).st_size == 0:
             json.dump([data], f)
         else:
-            with open(CURRENCY_RATES_FILE) as json_file:
-                data_list = json.load(json_file)
-            data_list.append(data)
             with open(CURRENCY_RATES_FILE, "w") as json_file:
-                json.dump(data_list, json_file)
+                json.dump(data, json_file)
+
+
+def load_from_json(file_name) -> dict:
+    with open(file_name, encoding='utf-8') as file:
+        content = file.read()
+        file_data = json.loads(content)
+    return file_data
+
+
+def print_time():
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    print("Время:", current_time)
 
 
 if __name__ == '__main__':
